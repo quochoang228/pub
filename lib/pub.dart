19df7299fba37cc5e8666892fbcc858b0844169c
quocht5 <quocht5@viettel.com.vn>
2025-01-06 10:25:23 +0700

    update other pubspec

--- lib/pub.dart
+++ lib/pub.dart
@@ -1,1 +1,40 @@
+export 'package:shared_preferences/shared_preferences.dart'
+    show
+        SharedPreferences,
+        SharedPreferencesAsync,
+        SharedPreferencesWithCache,
+        SharedPreferencesWithCacheOptions;
+
+export 'package:hooks_riverpod/hooks_riverpod.dart';
+
+export 'package:flutter_hooks/flutter_hooks.dart';
+
+export 'package:package_info_plus/package_info_plus.dart';
+
+export 'package:json_serializable/json_serializable.dart';
+export 'package:json_serializable/builder.dart';
+export 'package:json_serializable/type_helper.dart';
+
+export 'package:connectivity_plus/connectivity_plus.dart';
+
 export 'package:logging/logging.dart' show Level, LogRecord, Logger;
+
+export 'package:dio/dio.dart';
+export 'package:dio/browser.dart';
+export 'package:dio/io.dart';
+
+export 'package:sqflite/sqflite.dart';
+
+export 'package:get_it/get_it.dart';
+
+export 'package:go_router/go_router.dart';
+
+export 'package:gap/gap.dart';
+
+export 'package:uuid/uuid.dart';
+
+export 'package:firebase_analytics/firebase_analytics.dart';
+export 'package:firebase_messaging/firebase_messaging.dart';
+export 'package:firebase_core/firebase_core.dart';
+export 'package:firebase_crashlytics/firebase_crashlytics.dart';
+export 'package:flutter_local_notifications/flutter_local_notifications.dart';
